7ab94776d84bd68776baf7e6974d86430622b919
Guilherme Piccoli <guuipiccoli@gmail.com>
2020-04-13 17:17:18 -0300

    textfield design + navigationlink to search result screen (not working yet)

---
+++ LyricsPlayer/SearchResult/SearchResultView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchResultView.swift
+//  LyricsPlayer
+//
+//  Created by Guilherme Piccoli on 13/04/20.
+//  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchResultView()
+    }
+}
