dc8477126647ad81507add193c6a73b9abfed8e1
henriqueconte <henriqueconte6@gmail.com>
2020-04-14 17:34:14 -0300

    Started spotify connection

---
+++ LyricsPlayer/LyricsPlayer-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  LyricsPlayer-Bridging-Header.h
+//  LyricsPlayer
+//
+//  Created by Henrique Figueiredo Conte on 14/04/20.
+//  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
+//
+
+#ifndef LyricsPlayer_Bridging_Header_h
+#define LyricsPlayer_Bridging_Header_h
+
+
+#endif /* LyricsPlayer_Bridging_Header_h */
